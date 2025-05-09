cd5000b6f07ce97bfd5c07251494880544ca14e1
Pranjal Mishra <39010495+praanjalmishra@users.noreply.github.com>
2025-05-09 10:23:28 +0200

    Update setup.py

--- ros2_hc_drv/polar_ros2/setup.py
+++ ros2_hc_drv/polar_ros2/setup.py
@@ -1,14 +1,13 @@
 import os
 from glob import glob
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = 'polar_ros2'
-submodules = "polar_ros2/PolarH10"
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(include=[package_name, f'{package_name}.*']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
