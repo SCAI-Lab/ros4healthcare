c465d56061c5070d53f82816144b40e0c135d1c8
moperezs <moperez@ethz.ch>
2025-06-18 14:32:15 +0200

    Launch for demo

--- ros4hc_demo_full/launch/ros2_hc_launch.py
+++ ros4hc_demo_full/launch/ros2_hc_launch.py
@@ -6,31 +6,26 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     ld = LaunchDescription()
 
-    # Parameters for mbient_ros node
-    mbient_config = os.path.join(
-        get_package_share_directory('ros2_hc_launch'),
+    polar_config = os.path.join(
+        get_package_share_directory('polar_ros2'),
         'config',
         'params.yaml'
-    )
+        )
 
-    # Node for mbient_ros
-    mbient_node = Node(
-        package='mbient_ros',
-        executable='mbient_wrapper.py',
-        name='mbient_ros',
-        parameters=[mbient_config],
-        output='screen'
+    polar_node=Node(
+        package = 'polar_ros2',
+        name = 'polar_connector',
+        executable = 'polar_connector',
+        parameters = [polar_config]
     )
-    ld.add_action(mbient_node)
+    ld.add_action(polar_node)
 
-    # Parameters for sensomative_ros node
     sensomative_config = os.path.join(
         get_package_share_directory('ros2_hc_launch'),
         'config',  
         'params.yaml'
     )
 
-    # Node for sensomative_ros
     sensomative_node = Node(
         package='sensomative_ros',
         executable='sensomative_wrapper.py',
@@ -40,24 +35,12 @@ def generate_launch_description():
     )
     ld.add_action(sensomative_node)
 
-    # Node for healthcare_adl_classifier
-    adl_node = Node(
-        package='healthcare_adl_classifier',
-        executable='pub_adl',
-        name='adl_classifier',
-        parameters=[mbient_config],
+    m5_node = Node(
+        package='m5_udp_listener',
+        executable='m5_listener_multiple.py',
+        name='sensomative_ros',
         output='screen'
     )
-    ld.add_action(adl_node)
-
-    # Node for dashboard
-    dashboard_node = Node(
-        package='dashboard',
-        executable='dashboard',
-        name='dashboard',
-        parameters=[mbient_config],
-        output='screen'
-    )
-    ld.add_action(dashboard_node)
+    ld.add_action(m5_node)
 
     return ld
