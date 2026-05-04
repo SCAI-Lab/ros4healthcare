cb383ecfc192051413be4a9e317a9188e9178eeb
moperezs <moperez@ethz.ch>
2026-05-04 16:44:32 +0200

    Updated bleak command

--- ros2_hc_drv/polar_ros2/polar_ros2/submodules/PolarH10.py
+++ ros2_hc_drv/polar_ros2/polar_ros2/submodules/PolarH10.py
@@ -16,13 +16,38 @@ class PolarH10:
     PMD_CHAR2_UUID = "fb005c82-02e7-f387-1cad-8acd2d8df0c8"
 
     ECG_WRITE = bytearray([0x02, 0x00, 0x00, 0x01, 0x82, 0x00, 0x01, 0x01, 0x0E, 0x00])
-    ACC_WRITE = bytearray([0x02, 0x02, 0x00, 0x01, 0xC8, 0x00, 0x01, 0x01, 0x10, 0x00, 0x02, 0x01, 0x08, 0x00])
+    ACC_WRITE = bytearray(
+        [
+            0x02,
+            0x02,
+            0x00,
+            0x01,
+            0xC8,
+            0x00,
+            0x01,
+            0x01,
+            0x10,
+            0x00,
+            0x02,
+            0x01,
+            0x08,
+            0x00,
+        ]
+    )
 
     ACC_SAMPLING_FREQ = 200
     ECG_SAMPLING_FREQ = 130
     MAX_DATA_AGE_SEC = 1.0
 
-    def __init__(self, node, bleak_device, bt_adapter, publish_acceleration, publish_hr, publish_ecg):
+    def __init__(
+        self,
+        node,
+        bleak_device,
+        bt_adapter,
+        publish_acceleration,
+        publish_hr,
+        publish_ecg,
+    ):
         self.bleak_device = bleak_device
         self.bt_adapter = bt_adapter
         self.node = node
@@ -48,10 +73,14 @@ class PolarH10:
 
     async def connect(self):
         try:
-            self.bleak_client = BleakClient(self.bleak_device, adapter=self.bt_adapter)
+            self.bleak_client = BleakClient(
+                self.bleak_device,
+                adapter=self.bt_adapter,
+                disconnected_callback=self.handle_disconnect,
+            )
             await self.bleak_client.connect(timeout=20.0)
             print("Connected!")
-            self.bleak_client.set_disconnected_callback(self.handle_disconnect)
+
         except asyncio.TimeoutError:
             log.error("[PolarH10::connect] Connection failed. Retrying...")
             await self.connect()
@@ -66,14 +95,22 @@ class PolarH10:
             print("Disconnected.")
 
     async def start_combined_stream(self):
-        await self.bleak_client.write_gatt_char(self.PMD_CHAR1_UUID, self.ECG_WRITE, response=True)
-        await self.bleak_client.write_gatt_char(self.PMD_CHAR1_UUID, self.ACC_WRITE, response=True)
+        await self.bleak_client.write_gatt_char(
+            self.PMD_CHAR1_UUID, self.ECG_WRITE, response=True
+        )
+        await self.bleak_client.write_gatt_char(
+            self.PMD_CHAR1_UUID, self.ACC_WRITE, response=True
+        )
         await self.bleak_client.start_notify(self.PMD_CHAR2_UUID, self.pmd_data_conv)
         self.combined_streaming_active = True
         print("Streaming ECG and ACC...")
 
     async def stop_combined_stream(self):
-        if self.bleak_client and self.bleak_client.is_connected and self.combined_streaming_active:
+        if (
+            self.bleak_client
+            and self.bleak_client.is_connected
+            and self.combined_streaming_active
+        ):
             try:
                 await self.bleak_client.stop_notify(self.PMD_CHAR2_UUID)
                 print("Stopped streaming ECG and ACC.")
@@ -82,20 +119,24 @@ class PolarH10:
             self.combined_streaming_active = False
 
     async def start_hr_stream(self):
-        await self.bleak_client.start_notify(self.HEART_RATE_MEASUREMENT_UUID, self.hr_data_conv)
+        await self.bleak_client.start_notify(
+            self.HEART_RATE_MEASUREMENT_UUID, self.hr_data_conv
+        )
         self.hr_streaming_active = True
         print("Started HR stream.")
 
-
     async def stop_hr_stream(self):
-        if self.bleak_client and self.bleak_client.is_connected and self.hr_streaming_active:
+        if (
+            self.bleak_client
+            and self.bleak_client.is_connected
+            and self.hr_streaming_active
+        ):
             try:
                 await self.bleak_client.stop_notify(self.HEART_RATE_MEASUREMENT_UUID)
                 print("Stopped HR stream.")
             except Exception as e:
                 log.warning(f"[PolarH10] Failed to stop HR notify: {e}")
             self.hr_streaming_active = False
-
 
     def pmd_data_conv(self, sender, data):
         if data[0] == 0x00:
@@ -178,28 +219,26 @@ class PolarH10:
 
     @staticmethod
     def convert_array_to_signed_int(data, offset, length):
-        return int.from_bytes(data[offset:offset + length], byteorder="little", signed=True)
+        return int.from_bytes(
+            data[offset : offset + length], byteorder="little", signed=True
+        )
 
     @staticmethod
     def convert_to_unsigned_long(data, offset, length):
-        return int.from_bytes(data[offset:offset + length], byteorder="little", signed=False)
+        return int.from_bytes(
+            data[offset : offset + length], byteorder="little", signed=False
+        )
 
     def get_acc_data(self):
         times = np.array(self.acc_stream_times)
         if len(times) == 0:
             return None
         latest_time = times[-1]
-        return {
-            'times': times - times[0],
-            'values': np.array(self.acc_stream_values)
-        }
+        return {"times": times - times[0], "values": np.array(self.acc_stream_values)}
 
     def get_ibi_data(self):
         if not self.ibi_stream_times:
             return None
         times = np.array(self.ibi_stream_times)
         latest_time = times[-1]
-        return {
-            'times': times - times[0],
-            'values': np.array(self.ibi_stream_values)
-        }
+        return {"times": times - times[0], "values": np.array(self.ibi_stream_values)}
